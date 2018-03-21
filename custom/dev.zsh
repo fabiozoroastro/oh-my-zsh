37be89b23a0123233ba872d19e962610f86fb559
fa5u5m3 <Fabio_Souza@gap.com>
2018-03-21 16:50:53 -0700

    mend

---
+++ custom/dev.zsh
@@ -0,0 +1,6 @@
+########
+#### JAVA ALIASES
+########
+alias use_java8="export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)"
+alias use_java7="export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)"
+alias use_java9="export JAVA_HOME=$(/usr/libexec/java_home -v 9)"
